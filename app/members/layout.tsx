069511ebd286af52ed12bda52125b0990ca4b285
itc-s24016 <s24016@std.it-college.ac.jp>
2025-10-14 11:13:37 +0900

    誤字エラー修正

--- app/members/layout.tsx
+++ app/members/layout.tsx
@@ -1,5 +1,4 @@
-import styles from "@/app/_components/Sheet";
-import Sheet from "../_components/Sheet";
+import Sheet from "@/app/_components/Sheet";
 
 // members 配下のページに共通のレイアウトを提供する
 type Props = {
