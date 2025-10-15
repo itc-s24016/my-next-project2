98ad1533c451aca74c7830b86b89b30d28336e47
itc-s24016 <s24016@std.it-college.ac.jp>
2025-10-15 12:34:11 +0900

    microCMS と通信処理をする関数作成まで

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,10 +1,26 @@
+import { createClient } from "microcms-js-sdk";
+// microCMSから取得するデータの型を定義
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+// メンバーの型を定義
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
+// カテゴリの型を定義
 export type Category = {
-  // カテゴリの型を定義
   name: string;
 };
 
+// 記事の型を定義
 export type News = {
-  // 記事の型を定義
   id: string;
   title: string;
   category: {
@@ -12,4 +28,28 @@ export type News = {
   };
   publishedAt: string;
   createdAt: string;
+};
+
+// サービスドメインが設定されていない場合
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+// APIキーが設定されていない場合
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+// SDK に用意されている createClient を使ってクライアントを作成
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+// メンバー一覧を取得する関数
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
 };
